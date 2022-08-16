b37eca496043158a21a1d8a9ed6c969d6ecf47d6
RhonaJoyKe <rhonajoy8@gmail.com>
2022-08-16 17:02:38 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const trial= document.getElementById('main')
+trial.remove()
+const newHeader=document.createElement("h1")
+newHeader.setAttribute('id','victory')
+// newHeader.textContent='"YOUR-NAME is the champion'
+newHeader.innerHTML="YOUR-NAME is the champion"
